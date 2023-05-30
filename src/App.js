837895531f7a75b7609473dd0bacc662d2f8e549
afect13 <ns130192@gmail.com>
2023-05-30 11:16:11 +0500

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" text-center text-cyan-800">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
